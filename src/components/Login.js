c4c06be1128e74e9c80c9e5be3c293877fa60777
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2020-01-22 19:01:27 +0200

    login sivun teemailua

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
-import { FormControl, Input, Button } from '@material-ui/core'
+import { FormControl, OutlinedInput, Button, Grid, makeStyles } from '@material-ui/core'
 import loginService from '../services/login'
 
 const storageKey = 'loggedTabletopAppUser'
+
+const useStyles = makeStyles({
+  item: {
+    width: '11.5em'
+  }
+})
 
 const Login = ({ setUser, user }) => {
   const [username, setUsername] = useState('')
@@ -36,30 +42,59 @@ const Login = ({ setUser, user }) => {
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
-      <FormControl>
-        <Input
-          id='username'
-          value={username}
-          color='secondary'
-          placeholder='username'
-          onChange={({ target }) => setUsername(target.value)} />
-      </FormControl>
-      <FormControl>
-        <Input
-          id='password'
-          value={password}
-          type='password'
-          color='secondary'
-          placeholder='password'
-          onChange={({ target }) => setPassword(target.value)} />
-      </FormControl>
-      <Button variant='contained' color='secondary' type='submit'>login</Button>
+      <Grid
+        container
+        spacing={2}
+        justify='center'
+        alignItems='center'
+        direction='column'>
+        <Grid item>
+          <FormControl variant='outlined'>
+            <OutlinedInput
+              id='username'
+              value={username}
+              color='secondary'
+              placeholder='username'
+              style={{ backgroundColor: 'white', borderRadius: '40px' }}
+              inputProps={{ style: { textAlign: 'center' } }}
+              onChange={({ target }) => setUsername(target.value)} />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl variant='outlined'>
+            <OutlinedInput
+              id='password'
+              value={password}
+              type='password'
+              placeholder='password'
+              color='secondary'
+              style={{ backgroundColor: 'white', borderRadius: '40px' }}
+              inputProps={{ style: { textAlign: 'center' } }}
+              onChange={({ target }) => setPassword(target.value)} />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <Button
+            variant='contained'
+            color='secondary'
+            type='submit'
+            style={{ borderRadius: '40px', padding: '15px 30px', marginTop: '1em' }}>
+            login
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   )
 
   const logoutForm = () => (
     <form onSubmit={handleLogout}>
-      <Button variant='contained' color='secondary' type='submit'>logout</Button>
+      <Button
+        variant='contained'
+        color='secondary'
+        type='submit'
+        style={{ borderRadius: '40px', padding: '15px 30px', marginTop: '1em' }}>
+        logout
+      </Button>
     </form>
   )
 
